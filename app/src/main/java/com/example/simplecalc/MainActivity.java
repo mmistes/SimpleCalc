8f3536ff9357eac4efbf74f40fb1da5f2d5172f0
mmistes <93210581+mmistes@users.noreply.github.com>
2022-02-25 15:22:39 +0300

    Добавлено немного кода и фоновое изображение

--- app/src/main/java/com/example/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.simplecalc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result_field;
+    TextView operation_field;
+    EditText number_field;
+
+    Double operand = null;
+    String lastOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result_field.findViewById(R.id.result_field);
+        operation_field.findViewById(R.id.operation_field);
+        number_field.findViewById(R.id.number_field);
+
+    }
+    // сохранение состояния
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("OPERATION", lastOperation);
+
+        if(operand!=null) {
+            outState.putDouble("OPERATION", operand);
+        }
+    }
+    // получение ранее сохраненного состояния
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+
     }
 }
